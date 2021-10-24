4a5d4ec67f94e98bb5231aecc9effb73abdf9bac
arielmell0 <arielmello@rede.ulbra.br>
2021-10-24 18:04:21 -0300

    feat: fatorial exercicio
    exercicio usando recursão para calcular a fatorial de algum número

---
+++ exercicio-1.c
@@ -0,0 +1,18 @@
+// Escreva uma função em C que calcule o fatorial de um número inteiro.
+// Crie um programa que use essa função
+
+#include <stdio.h>
+
+int fact(int x) {
+	if (x == 1)
+		return 1;
+	else
+		return x * fact(x - 1);
+}
+
+int main(void) {
+	
+	printf("%d", fact(5));
+
+	return 0;
+}

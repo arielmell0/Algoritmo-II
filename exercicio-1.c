4b183a90b1d30d05a55cdd218294226796755bd7
arielmell0 <arielmello@rede.ulbra.br>
2021-10-24 20:49:06 -0300

    feat: finished program

--- exercicio-1.c
+++ exercicio-1.c
@@ -2,6 +2,7 @@
 // Crie um programa que use essa função
 
 #include <stdio.h>
+#include <locale.h>
 
 int fact(int x) {
 	if (x == 1)
@@ -10,9 +11,13 @@ int fact(int x) {
 		return x * fact(x - 1);
 }
 
-int main(void) {
-	
-	printf("%d", fact(5));
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+    int fatInput;
+    printf("Calculo de fatorial\n---------------------\n\nDigite aqui um número: ");
+    scanf("%i", &fatInput);
+
+	printf("%d""! = ""%d",fatInput, fact(fatInput));
 
 	return 0;
 }
